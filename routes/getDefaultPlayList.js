1d38e6dd6484c56d114337f388ccfc17b116129c
QUICKLyas <156132264@qq.com>
2021-11-23 08:27:38 +0800

    直接在js中完成目的

--- routes/getDefaultPlayList.js
+++ routes/getDefaultPlayList.js
@@ -4,44 +4,30 @@ const { spawn } = require("child_process");
 var fs = require("fs");
 const cors = require('cors')
 const connectMusicDB = require("../components/mongoConnection");
-const runPython = require("../components/runPython");
+// const runPython = require("../components/runPython");
 
 router.use(express.json())
 router.use(express.urlencoded({ extended: false }))
 router.use(cors())
 
 // 模糊查询歌曲名
-router.post("/", (req, res, next) => {
-  // 判断POST请求是否包含请求体
-  try {
-    runPython("./pyprog/mongoRandomPlayList.py", () => {
-      const data = fs.readFileSync('./random_playlist.txt', { encoding: 'utf-8' })
-      const music_result_arr = data.split('\n')
-      let music_result_json = []
-      music_result_arr.map(item => {
-        music_result_json.push(JSON.parse(item))
-      })
-      // 发送JSON
-      res.send({ success: true, result: music_result_json })
-      fs.unlink('./random_playlist.txt', (err) => {
-        if (err) throw err;
-      });
-    })
-  } catch (error) {
-    res.send({ success: false }).status(404);
-    next();
-  }
+router.post("/", async (req, res, next) => {
+    // 判断POST请求是否包含请求体
+    const body = req.body;
+        // 连接数据库
+    const MusicDB = await connectMusicDB();
+        // 数据库操作需要异步
+    const catchPLByCat = await MusicDB.collection("MusicPlayList");
+    let collectData = await catchPLByCat.aggregate(
+      [ 
+        {$sample: {size: 15}},
+      ]).toArray();
+    if (collectData.length) {
+        res.send({ success: true, data: collectData}).status(200)
+      } else {
+        res.send({ success: false, data: "",message: "对象不存在"}).status(200)
+      }
+      next();
 });
-
-// router.post('/randomPlayList', async (req, res, next) => {
-//   try {
-//     const musicDB = await connectMusicDB();
-//     const allMusicCollection = await musicDB.collection("MusicPlayList")
-//     const musicData = await allMusicCollection.aggregate([{ $sample: { size: 20 } }]).toArray()
-//     res.send({ success: true, musics: musicData })
-//   } catch (error) {
-//     res.send({ success: false }).status(404)
-//   }
-// })
 
 module.exports = router;
--- routes/getPlayListCat.js
+++ routes/getPlayListCat.js
@@ -4,34 +4,36 @@ const { spawn } = require("child_process");
 var fs = require("fs");
 const cors = require('cors')
 const connectMusicDB = require("../components/mongoConnection");
-const runPython = require("../components/runPython");
+// const runPython = require("../components/runPython");
 
 router.use(express.json())
 router.use(express.urlencoded({ extended: false }))
 router.use(cors())
 
 // 模糊查询歌曲名
-router.post("/", (req, res, next) => {
-  // 判断POST请求是否包含请求体
-  if (Object.keys(req.body).length) {
-    const body = req.body;
-    fs.writeFileSync("music_cat.json", JSON.stringify(body));
-    runPython("./pyprog/mongoClassify.py", () => {
-      const data = fs.readFileSync('./music_playlist_data.txt', { encoding: 'utf-8' })
-      const music_result_arr = data.split('\n')
-      let music_result_json = []
-      music_result_arr.map(item => {
-        music_result_json.push(JSON.parse(item))
-      })
-      // 发送JSON
-      res.send({ success: true, result: music_result_json })
-      fs.unlink('./music_playlist_data.txt', (err) => {
-        if (err) throw err;
-      });
-    })
-  } else {
-    res.send({ errmsg: "request Body为空" }).status(404);
-    next();
-  }
+router.post("/", async (req, res, next) => {
+    // 判断POST请求是否包含请求体
+    if (Object.keys(req.body).length) {
+      const body = req.body;
+        // 连接数据库
+      const MusicDB = await connectMusicDB();
+        // 数据库操作需要异步
+      const catchPLByCat = await MusicDB.collection("MusicPlayList");
+      let collectData = await catchPLByCat.aggregate(
+        [ 
+          {$match: {tags:{ $all: [body.tags]}}},
+          {$sample: {size: 90}},
+          {$sort:{tags:-1}}       
+        ]).toArray();
+      if (collectData.length) {
+          res.send({ success: true, data: collectData}).status(200)
+        } else {
+          res.send({ success: false, data: "",message: "对象不存在"}).status(200)
+        }
+      } else {
+            res.send({ errmsg: "request Body为空" }).status(404);
+        next();
+    }
 });
+
 module.exports = router;
